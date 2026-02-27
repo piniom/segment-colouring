93d1341374604916af0cb70f22d0ae792bf5ee66
piniom <szymon.wojtulewicz@gmail.com>
2026-02-27 22:13:28 +0100

    bitmaps strategy start

--- rust/src/bin/test_bitmaps.rs
+++ rust/src/bin/test_bitmaps.rs
@@ -1,7 +1,7 @@
 use segment_colouring::simple_state::state::State;
 
 fn main() {
-    let state = State::<4>::new();
+    let state = State::<3>::new();
     let result = state.generate_all(10);
     println!("{}", result.len());
 }
--- rust/src/simple_state/mod.rs
+++ rust/src/simple_state/mod.rs
@@ -1,5 +1,35 @@
+use ahash::HashMap;
+
+use crate::simple_state::state::State;
+
 pub mod state;
 
-// impl State {
-//     fn find_strateg
-// }
+pub type Move = (u8, u8);
+
+#[derive(Debug, Default)]
+pub enum Visited {
+    #[default]
+    No,
+    Active,
+    Winning(Move)
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct StateWithMove<const MAX_CLIQUE: u32>  {
+    state: State<MAX_CLIQUE>,
+    move_: Move
+}
+
+impl<const MAX_CLIQUE: u32> StateWithMove<MAX_CLIQUE>{
+    pub fn allowed_colours_count(&self) -> u8 {
+        self.state.allowed_colours_for_segment(self.move_.0, self.move_.1).count_ones() as u8
+    }
+    fn find_strategy(&self, map: &mut HashMap<Self, Visited>) {
+
+    }
+}
+
+impl<const MAX_CLIQUE: u32> State<MAX_CLIQUE>{
+    fn find_strategy(&self, map: &mut HashMap<Self, Visited>) {
+
+    }
+}
