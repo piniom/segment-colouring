e9560c22bca11a832c55faa84dfd72aaffdf1d33
piniom <szymon.wojtulewicz@gmail.com>
2026-05-18 20:12:19 +0200

    more printing improvements

--- rust/src/simple_state/find/root.rs
+++ rust/src/simple_state/find/root.rs
@@ -1,21 +1,58 @@
 use crate::simple_state::state::State;
 use chrono::Utc;
-use std::io::{self, Write};
-use std::time::{Duration, Instant};
+use std::io::Write;
+use std::sync::mpsc;
+use std::{io, thread};
+use std::time::Instant;
 
 use super::{FindResult, SearchState};
 
 impl<const MAX_CLIQUE: u32> State<MAX_CLIQUE> {
-    fn format_elapsed(elapsed: Duration) -> String {
-        let secs = elapsed.as_secs_f64();
-        if secs >= 3600.0 {
-            format!("{:.2}h", secs / 3600.0)
-        } else if secs >= 60.0 {
-            format!("{:.2}m", secs / 60.0)
-        } else if secs >= 1.0 {
-            format!("{:.2}s", secs)
+    fn spawn_printer<'scope, 'env>(
+        scope: &'scope thread::Scope<'scope, 'env>,
+        rx: mpsc::Receiver<CountMessage>,
+        search_state: &'env SearchState<MAX_CLIQUE>,
+    ) -> thread::ScopedJoinHandle<'scope, ()>
+    where
+        'env: 'scope,
+    {
+        scope.spawn(|| {
+            let mut previous_winning = 0;
+            print_headers();
+            for message in rx {
+                match message {
+                    CountMessage::Row => {
+                        let stats = search_state.count_stats();
+                        let delta = stats.winning_states - previous_winning;
+                        println!(
+                            "{:<22} {:<22} {:<22} {}/{}",
+                            stats.winning_states,
+                            delta,
+                            stats.winning_substates,
+                            stats.total_states,
+                            stats.total_substates
+                        );
+                        previous_winning = stats.winning_states;
+                    }
+                }
+            }
+        })
+    }
+
+    fn format_elapsed(elapsed: chrono::Duration) -> String {
+        let secs = elapsed.num_seconds();
+        if secs >= 3600 {
+            let hours = secs / 3600;
+            let minutes = (secs % 3600) / 60;
+            format!("{:02}:{:02}", hours, minutes)
+        } else if secs >= 60 {
+            let minutes = secs / 60;
+            let seconds = secs % 60;
+            format!("{:02}:{:02}", minutes, seconds)
+        } else if secs >= 1 {
+            format!("00:{:02}", secs)
         } else {
-            format!("{}ms", elapsed.as_millis())
+            format!("{}ms", elapsed.subsec_millis())
         }
     }
 
@@ -29,60 +66,64 @@ impl<const MAX_CLIQUE: u32> State<MAX_CLIQUE> {
         depth: usize,
         max_size: u8,
     ) -> FindResult {
-        let mut previous_winning = 0;
         let total_start = Instant::now();
-        println!();
-        println!(
-            "{:<6} {:<12} {:<14} {:<10} {:<22} {:<22} {:<22} {}",
-            "depth",
-            "time",
-            "elapsed",
-            "result",
-            "w_states",
-            "delta",
-            "w_substates",
-            "t_states/t_substates"
-        );
-        for d in 3..=depth {
-            print!("{:<6} {:<12} ", d, Self::format_time_now());
-            let _ = io::stdout().flush();
-            let start = Instant::now();
-            let result = self.find_strategy(search_state, d, max_size);
-            let elapsed = Self::format_elapsed(start.elapsed());
-            let result_label = match result {
-                FindResult::Winning => "win",
-                FindResult::Losing => "lose",
-            };
-            print!("{:<14} {:<10} ", elapsed, result_label);
-            let _ = io::stdout().flush();
-            let stats = search_state.count_stats();
-            let delta = stats.winning_states - previous_winning;
+        let (tx, rx) = mpsc::channel::<CountMessage>();
+        let mut final_result = FindResult::Losing;
 
-            println!(
-                "{:<22} {:<22} {:<22} {}/{}",
-                stats.winning_states,
-                delta,
-                stats.winning_substates,
-                stats.total_states,
-                stats.total_substates
-            );
-            previous_winning = stats.winning_states;
-            if let w @ FindResult::Winning = result {
-                println!(
-                    "{:<6} {:<12} {:<14}",
-                    "total",
-                    Self::format_time_now(),
-                    Self::format_elapsed(total_start.elapsed())
-                );
-                return w;
+        thread::scope(|s| {
+            let printer = Self::spawn_printer(s, rx, search_state);
+
+            for d in 3..=depth {
+                let time = Self::format_time_now();
+                let start = Instant::now();
+                let result = self.find_strategy(search_state, d, max_size);
+                let elapsed = chrono::Duration::from_std(start.elapsed())
+                    .unwrap_or_else(|_| chrono::Duration::zero());
+                let elapsed = Self::format_elapsed(elapsed);
+                let result_label = match result {
+                    FindResult::Winning => "win",
+                    FindResult::Losing => "lose",
+                };
+                print!("{:<6} {:<12} {:<14} {:<10} ", d, time, elapsed, result_label);
+                let _ = io::stdout().flush();
+                let _ = tx.send(CountMessage::Row);
+                if let FindResult::Winning = result {
+                    final_result = FindResult::Winning;
+                    break;
+                }
             }
-        }
+            drop(tx);
+            let _ = printer.join();
+        });
+
         println!(
             "{:<6} {:<12} {:<14}",
             "total",
             Self::format_time_now(),
-            Self::format_elapsed(total_start.elapsed())
+            Self::format_elapsed(
+                chrono::Duration::from_std(total_start.elapsed())
+                    .unwrap_or_else(|_| chrono::Duration::zero())
+            )
         );
-        FindResult::Losing
+        final_result
     }
+}
+
+fn print_headers() {
+    println!();
+    println!(
+        "{:<6} {:<12} {:<14} {:<10} {:<22} {:<22} {:<22} {}",
+        "depth",
+        "time",
+        "elapsed",
+        "result",
+        "w_states",
+        "delta",
+        "w_substates",
+        "t_states/t_substates"
+    );
+}
+
+enum CountMessage {
+    Row,
 }
