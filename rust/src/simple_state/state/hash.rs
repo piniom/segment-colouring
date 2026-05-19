a0e75ed91d21c785d587779b04bfcdfec961c1f3
piniom <szymon.wojtulewicz@gmail.com>
2026-05-20 00:23:01 +0200

    idiomatic hash

--- rust/src/simple_state/state/hash.rs
+++ rust/src/simple_state/state/hash.rs
@@ -1,14 +1,10 @@
 use super::*;
 use std::{
     hash::{Hash, Hasher},
-    mem,
 };
 
 impl<const MAX_CLIQUE: u32> Hash for State<MAX_CLIQUE> {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        let bytes = unsafe {
-            std::slice::from_raw_parts((self as *const Self) as *const u8, mem::size_of::<Self>())
-        };
-        state.write(bytes);
+        state.write_u128(self.data);
     }
 }
